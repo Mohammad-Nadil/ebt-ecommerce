c976f2884d71faa26afec5e46926147e45766f1e
mohammad-nadil <mdhossain.dev@gmail.com>
2026-02-08 15:32:48 +0600

    setup complete

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.jsx";
 import { BrowserRouter, Route, Router, Routes } from "react-router";
+import Product from "./pages/Product.jsx";
+import ProductDetail from "./pages/ProductDetail.jsx";
+import Home from "./Home.jsx";
 console.log("asdfasdf");
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route index element={<App />} />
+        <Route index element={<Home />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/product/:id" element={<ProductDetail />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
