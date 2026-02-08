fb4e967d6416c0e02c0b476b73e48cebf5662c47
mohammad-nadil <mdhossain.dev@gmail.com>
2026-02-08 15:11:04 +0600

    console added in main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter, Route, Router, Routes } from "react-router";
+console.log("asdfasdf");
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
