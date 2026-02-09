f88c150f6952ec75836c4c9b3825556df9bbee3e
“fima0519” <fimafaria@gmail.com>
2026-02-10 00:37:45 +0600

    Added Dynamic Rating Bar

--- src/components/RatingBar.jsx
+++ src/components/RatingBar.jsx
@@ -3,22 +3,22 @@ import Flex from "./Flex";
 
 const RatingBar = ({ stars, percent }) => {
   return (
-    <Flex className="items-center gap-x-4">
+    <Flex className="flex-col sm:flex-row sm:items-center gap-2 sm:gap-x-4 w-full">
       {/* star label */}
-      <span className="text-sm w-[60px]">
+      <span className="text-sm w-full sm:w-[60px] shrink-0">
         {stars} Stars
       </span>
 
       {/* progress bar */}
-      <div className="w-[200px] h-2 bg-gray-200 rounded">
+      <div className="w-full sm:w-[200px] h-2 bg-gray-200 rounded overflow-hidden">
         <div
           className="h-full bg-starColor rounded transition-all duration-500"
           style={{ width: `${percent}%` }}
-        ></div>
+        />
       </div>
 
       {/* percentage */}
-      <span className="text-sm w-[40px]">
+      <span className="text-sm w-full sm:w-[40px] text-right sm:text-left shrink-0">
         {percent}%
       </span>
     </Flex>
