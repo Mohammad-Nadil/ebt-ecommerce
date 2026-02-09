f976a3146ac322fa7ee78a591ee4f1b4bca841c5
mohammad-nadil <mdhossain.dev@gmail.com>
2026-02-09 14:21:18 +0600

    home components created

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Container from "./components/Container.jsx"
+import Banner from './components/Banner'
+import Category from './components/Category'
+import FlashDeals from './components/FlashDeals'
+import FeatureProducts from './components/FeatureProducts'
 
 const Home = () => {
   return (
     <>
-    <div>Home</div>
+    <div>
+      <Container>
+        <Banner/>
+        <Category/>
+        <FlashDeals/>
+        <FeatureProducts/>
+      </Container>
+    </div>
     </>
 
   )
